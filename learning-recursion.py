188a663429ddf70dc085d22312d2f081843aceb0
rushikesh-dange <rushikesh6251@gmail.com>
2025-12-25 16:02:10 +0530

    Day 4- learning basic examples using recursion

--- learning-recursion.py
+++ learning-recursion.py
@@ -1,6 +1,49 @@
+count = 0
+sum=0
 def f():
     # this shows infinite recursion
     print("1")
     f()
+def f1():
+    global count
+    if count ==4 :
+        return
+    print(count)
+    count+=1
+    f1()
+def pname(i,n):
+    global count
+    if i > n:
+        return
+    print("Rushikesh")
+    pname(i+1,n)
+def pnum(i,n):
+    if i>n:
+        return
+    print(i)
+    pnum(i+1,n)
+def prnum(i,n):
+    if i<1:
+        return
+    print(i)
+    prnum(i-1,n)
+def psum(i,n):
+    global sum
+    if i > n:
+        print("the sum of N numbers is: ", sum)
+        return
+    sum = sum + i
+    psum(i + 1, n)
+def btpnum(i,n):
+    if i <1:
+        return
+    btpnum(i-1,n)
+    print(i)
+def btprnum(i,n):
+    if i >n:
+        return
+    btprnum(i+1,n)
+    print(i)
 if __name__=="__main__":
-    f()
+    n=int(input("enter any integer: "))
+    btprnum(1,n)
