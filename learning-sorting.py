beb1c316883b3d5da08517e8e8c2f5f554119b2c
rushikesh-dange <rushikesh6251@gmail.com>
2026-01-12 18:02:22 +0530

    Day 12 – Learning merge sort

--- learning-sorting.py
+++ learning-sorting.py
@@ -25,9 +25,38 @@ def insertion(a):
             a[j-1],a[j]=a[j],a[j-1]
             j-=1
     print(a)
+
+def ms(a,low,high):
+    if low>=high:
+        return
+    mid=(low+high)//2
+    ms(a,low,mid)
+    ms(a,mid+1,high)
+    merge(a,low,mid,high)
+
+def merge(a,low,mid,high):
+    temp=[]
+    left=low
+    right=mid+1
+    while left<=mid and right<=high:
+        if a[left]<=a[right]:
+            temp.append(a[left])
+            left+=1
+        else:
+            temp.append(a[right])
+            right+=1
+    while left<=mid:
+        temp.append(a[left])
+        left += 1
+    while right<=mid:
+        temp.append(a[right])
+        right += 1
+    for i in range(low,high+1):
+        a[i]=temp[i-low]
+
 if __name__=="__main__":
     n=int(input("enter size of array: "))
     a=[]
     for i in range(n):
         a.append(int(input("enter array element: ")))
-    insertion(a)
+    ms(a,0,len(a)-1)
